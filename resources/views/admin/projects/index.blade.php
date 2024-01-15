f1f2b61caa2a269147ccdad2561465bb3b23ce0d
iaio998 <depalatisiacopo@gmail.com>
2024-01-15 11:30:34 +0100

    Printed table in index projects

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -4,21 +4,43 @@
     <div class="row">
         <h1>Index</h1>
         <button class="btn btn-success"><a href="{{route('admin.projects.create')}}">Create new</a></button>
-        @foreach($projects as $project)
-        <div class="col-4 col-md-4 text-center g-2">
-            <div class="img-cont">
-                <img src="{{ asset('storage/' . $project->image) }}" alt="{{$project->title}}">
-            </div>
-            <div class="">
-                <h2 class="fs-4">
-                    {{ $project->title }}
-                </h2>
-                <p>Description: {{ $project->body }}</p>
-                <p><a href="{{$project->url}}">Link to projeck</a></p>
-                <a class="btn btn-primary" href="{{route('admin.projects.show', $project)}}">Show more</a>
-            </div>
+        <div class="col-12">
+            <table class="my-3">
+                <tr>
+                    <th>Title</th>
+                    <th>Description</th>
+                    <th>Image</th>
+                    <th>Url</th>
+                    <th>Actions</th>
+                </tr>
+                @foreach($projects as $project)
+                <tr>
+                    <td>
+                        {{$project->title}}
+                    </td>
+                    <td>
+                        {{$project->body}}
+                    </td>
+                    <td>
+                        <div class="img-cont"><img src="{{ asset('storage/' . $project->image) }}"
+                                alt="{{$project->title}}"></div>
+                    </td>
+                    <td>
+                        <a href="{{$project->url}}">Link to projeck</a>
+                    </td>
+                    <td>
+                        <a class="btn btn-primary" href="{{route('admin.projects.show', $project)}}">Show</a>
+                        <a class="btn btn-primary" href="{{route('admin.projects.edit', $project)}}">Edit</a>
+                        <form action="{{route('admin.projects.destroy', $project)}}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+                @endforeach
+            </table>
         </div>
-        @endforeach
     </div>
 </div>
 @endsection
